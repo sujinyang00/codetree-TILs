c8991cf8bd790590d5edccfa18777f87bcc40882
sujinyang00 <68004188+sujinyang00@users.noreply.github.com>
2024-07-21 04:31:49 +0900

    Title: 알파벳에 따른 평가, Time: 99ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240721/알파벳에 따른 평가/evaluation-by-alphabet.py
@@ -0,0 +1,13 @@
+ch = input()
+rst = 'Failure'
+if ch == 'S' :
+    rst = 'Superior'
+elif ch == 'A':
+    rst = 'Excellent'
+elif ch == 'B' :
+    rst = 'Good'
+elif ch == 'C' :
+    rst = 'Usually'
+elif ch == 'D' :
+    rst == 'Effort'
+print(rst)
